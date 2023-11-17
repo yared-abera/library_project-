6ab8726e42982966e7f60b49b7aecce3c7022ac7
yared-abera <yaredoabera7@gmail.com>
2023-11-17 09:44:39 -0800

    i update person class

--- Student.java
+++ Student.java
@@ -1,10 +1,18 @@
 public class Student extends Person {
-      String classroom;
-
+     Classroom classroom;
 
     public Student(int age, boolean parentPermission, String classroom, String name) {
         super(age, parentPermission, name);
-        this.classroom = classroom;
+        this.classroom = new Classroom(classroom);
+        this.classroom.addStudent(this);
+    }
 
+    public Classroom getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(Classroom classroom) {
+        this.classroom = classroom;
+        classroom.addStudent(this);
     }
 }
