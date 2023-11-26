b510df2b7f5d874c4ed151f6a383ca2fc889ead2
yared-abera <140695462+yared-abera@users.noreply.github.com>
2023-11-25 23:37:17 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         App app = new App();
 
         while (true) {
-            System.out.println("Choose an option:");
+              System.out.println("Choose an option:");
             System.out.println("1. List all books");
             System.out.println("2. List all people");
             System.out.println("3. Create a person");
